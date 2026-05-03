6cba457a6706d8c4ecdcd5ae645d412fc7538e50
Rodrigo Callealta <lironscallealta@gmail.com>
2026-05-03 18:26:59 -0400

    add initial models for Especie and Raza

---
+++ src/main/java/cl/duoc/api_mascotas/model/Especie.java
@@ -0,0 +1,26 @@
+package cl.duoc.api_mascotas.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "especies")
+public class Especie {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long idEspecie;
+    String nombreEspecie;
+    String DescripcionEspecie;
+
+
+}
--- src/main/java/cl/duoc/api_mascotas/model/Mascota.java
+++ src/main/java/cl/duoc/api_mascotas/model/Mascota.java
@@ -1,5 +1,45 @@
 package cl.duoc.api_mascotas.model;
 
+import java.time.LocalDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class Mascota {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    
+    @Column(length = 50, nullable = false)
+    private String nombreMascota;
+
+    
+    @Column(nullable = false)
+    private Long idDueno;
+
+    @Column(name = "fecha_nacimiento", nullable = false)
+    private LocalDate fechaNacimientoMascota;
+
+    @Column(length = 30)
+    private Especie espacie;
+    // * hacer clasep para tabla
+
+    @Column(length = 30)
+    private Raza raza;
+   
+
 
 }
