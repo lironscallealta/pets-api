c975e2777135dcf45c814d0f2faf1e96334eab81
Rodrigo Callealta <lironscallealta@gmail.com>
2026-05-19 22:01:16 -0400

    feat: implement MascotaService to handle CRUD operations for pet management

--- src/main/java/cl/duoc/api_mascotas/service/MascotaService.java
+++ src/main/java/cl/duoc/api_mascotas/service/MascotaService.java
@@ -10,10 +10,9 @@ import cl.duoc.api_mascotas.dto.request.MascotaRequestDTO;
 import cl.duoc.api_mascotas.dto.response.EspecieResponseDTO;
 import cl.duoc.api_mascotas.dto.response.MascotaResponseDTO;
 import cl.duoc.api_mascotas.dto.response.RazaResponseDTO;
-// import cl.duoc.api_mascotas.dto.response.UsuarioResponseDTO;
+import cl.duoc.api_mascotas.exception.ResourceNotFoundException;
 import cl.duoc.api_mascotas.model.Mascota;
 import cl.duoc.api_mascotas.model.Raza;
-// import cl.duoc.api_mascotas.repository.EspecieRepository;
 import cl.duoc.api_mascotas.repository.MascotaRepository;
 import cl.duoc.api_mascotas.repository.RazaRepository;
 import java.util.ArrayList;
@@ -22,6 +21,8 @@ import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+// import cl.duoc.api_mascotas.dto.response.UsuarioResponseDTO;
+// import cl.duoc.api_mascotas.repository.EspecieRepository;
 
 @Service
 @RequiredArgsConstructor
@@ -29,7 +30,6 @@ public class MascotaService {
 
     private final MascotaRepository mascotaRepository;
     private final RazaRepository razaRepository;
-    // private final EspecieRepository especieRepository;
 
     private MascotaResponseDTO mapToMascotaToMascotaResponse(Mascota mascotaModel) {
 
@@ -42,39 +42,35 @@ public class MascotaService {
 
         // Seteando Especie
         especieResponse.setId(mascotaModel.getRaza().getEspecie().getId());
-        especieResponse.setNombreEspecie(mascotaModel.getRaza().getEspecie().getNombreEspecie()); // EJ PERRO
+        especieResponse.setNombreEspecie(mascotaModel.getRaza().getEspecie().getNombreEspecie());
         especieResponse.setDescripcionEspecie(
-                mascotaModel.getRaza().getEspecie().getDescripcionEspecie()); // -> abajo
-        especieResponse.setExoticaBoolean(mascotaModel.getRaza().getEspecie().getExoticaBoolean()); // Si fuese iguana
-        // poner true por
-        // EJ Mamífero carnívoro subespecie del lobo, animal domestico por excelencia.
+                mascotaModel.getRaza().getEspecie().getDescripcionEspecie());
+        especieResponse.setExoticaBoolean(mascotaModel.getRaza().getEspecie().getExoticaBoolean());
 
         // Seteando Raza
         razaResponse.setId(mascotaModel.getRaza().getId());
-        razaResponse.setNombreRaza(mascotaModel.getRaza().getNombreRaza()); // EJ BULLDOG
-        razaResponse.setDescripcionRaza(mascotaModel.getRaza().getDescripcionRaza()); // Ej Complexión robusta y
-        // compacta originaria del Reino
-        // Unido.
+        razaResponse.setNombreRaza(mascotaModel.getRaza().getNombreRaza());
+        razaResponse.setDescripcionRaza(mascotaModel.getRaza().getDescripcionRaza());
         razaResponse.setEspecieResponse(especieResponse);
 
         // Seteando Clase Maestra
         mascotaResponse.setId(mascotaModel.getId());
-        mascotaResponse.setNombreMascota(mascotaModel.getNombreMascota()); // Pello
+        mascotaResponse.setNombreMascota(mascotaModel.getNombreMascota());
         mascotaResponse.setFechaNacimientoMascota(mascotaModel.getFechaNacimientoMascota());
-        mascotaResponse.setEsDocilBoolean(mascotaModel.getEsDocilBoolean()); // informacion para saber si es agresivo o
-        // no importante si es que cambian doctor
-        // por ejm
-        mascotaResponse.setRazaResponse(razaResponse); // Objeto
-        mascotaResponse.setIdCliente(null); // este va con client
+        mascotaResponse.setEsDocilBoolean(mascotaModel.getEsDocilBoolean());
+        mascotaResponse.setRazaResponse(razaResponse);
+        mascotaResponse.setIdCliente(null);
 
         return mascotaResponse;
     }
 
+    @Transactional
     public MascotaResponseDTO registrarMascota(MascotaRequestDTO mascotaRequest) {
 
         Raza razaRegistrar = razaRepository
                 .findById(mascotaRequest.getIdRaza())
-                .orElseThrow(() -> new RuntimeException("raza no existente"));
+                .orElseThrow(() ->
+                        new ResourceNotFoundException("La raza con ID " + mascotaRequest.getIdRaza() + " no existe"));
 
         Mascota mascotaModel = new Mascota();
 
@@ -93,8 +89,9 @@ public class MascotaService {
 
     public Optional<MascotaResponseDTO> consultarMascotaId(Long idMascota) {
 
-        Mascota mascotaEncontrarId = mascotaRepository.findById(idMascota).orElseThrow(); // verlo despues con los
-        // errores
+        Mascota mascotaEncontrarId = mascotaRepository
+                .findById(idMascota)
+                .orElseThrow(() -> new ResourceNotFoundException("No se encontró la mascota con ID: " + idMascota));
         MascotaResponseDTO mascotaResponse = mapToMascotaToMascotaResponse(mascotaEncontrarId);
         return Optional.of(mascotaResponse);
     }
@@ -110,7 +107,6 @@ public class MascotaService {
 
     }
 
-    // Metodo hecho de la forma que siempre he usado
     public List<MascotaResponseDTO> consultarMascotasdos() {
 
         List<MascotaResponseDTO> mascotaResponseList = new ArrayList<>();
@@ -124,11 +120,12 @@ public class MascotaService {
         return mascotaResponseList;
     }
 
-    @Transactional // lo escribi por que no me guardaba los set. Probablememente escribirlo en los
-    // metodos eliminar y crear despues
+    @Transactional
     public Optional<MascotaResponseDTO> actualizarMascota(Long idMascota, MascotaRequestDTO nuevosDatos) {
 
-        Mascota mascotaModificar = mascotaRepository.findById(idMascota).orElseThrow(); // modificar despues con error
+        Mascota mascotaModificar = mascotaRepository
+                .findById(idMascota)
+                .orElseThrow(() -> new ResourceNotFoundException("no se encontro la mascota con id: " + idMascota));
         mascotaModificar.setNombreMascota(nuevosDatos.getNombreMascota());
         mascotaModificar.setFechaNacimientoMascota(nuevosDatos.getFechaNacimientoMascota());
         mascotaModificar.setEsDocilBoolean(nuevosDatos.getEsDocilBoolean());
@@ -140,32 +137,10 @@ public class MascotaService {
     @Transactional
     public void eliminarMascotaId(Long idMascota) {
 
-        Mascota mascotaEliminar = mascotaRepository.findById(idMascota).orElseThrow();
+        Mascota mascotaEliminar = mascotaRepository
+                .findById(idMascota)
+                .orElseThrow(() -> new ResourceNotFoundException(
+                        "no se puede eliminar porque no existe la mascota con id: " + idMascota));
         mascotaRepository.delete(mascotaEliminar);
     }
-
-    /*
-     * public Optional<MascotaResponseDTO> eliminarMascotaId(Long idMascota) {
-     * Optional<Mascota> mascotaEliminar = mascotaRepository.findById(idMascota);
-     * if (mascotaEliminar.isPresent()) {
-     *
-     * }
-     * MascotaResponseDTO mascotaResponse =
-     * mapToMascotaToMascotaResponse(mascotaEliminar);
-     * mascotaRepository.deleteById(idMascota);
-     * return Optional.of(mascotaResponse);
-     *
-     * }
-     */
 }
-
-/*
- * Notas
- * public MascotaResponseDTO registrarMascota(MascotaRequestDTO mascotaRequest)
- * {
- *
- * // Especie especieRegistrar = razaRegistrar.getEspecie(); // podemos omitir
- * esto
- * // ya que raza y especie van ligadas, pero para entender mejor el codigo esta
- * // asi
- */
