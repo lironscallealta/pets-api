20a3661fa085f37b1334313780413e359cbcd610
lironscallealta <lironscallealta@gmail.com>
2026-06-25 19:22:09 -0400

    feat(config): add OpenApiConfig class for Swagger documentation

---
+++ src/main/java/cl/duoc/api_mascotas/config/OpenApiConfig.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright © 2026 DuocUC FullStack 1
+ * Eduardo Bray
+ * Rodrigo Callealta
+ * Fernando Villalobos
+ */
+package cl.duoc.api_mascotas.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    private static final String SECURITY_SCHEME_NAME = "bearerAuth";
+
+    @Bean
+    public OpenAPI petsOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Pets Microservice API")
+                        .version("v1")
+                        .description("Microservicio de gestion de mascotas, razas y especies.")
+                        .contact(new Contact().name("DuocUC FullStack 1").email("soporte@duoc.cl"))
+                        .license(new License().name("MIT").url("https://opensource.org/licenses/MIT")))
+                .addSecurityItem(new SecurityRequirement().addList(SECURITY_SCHEME_NAME))
+                .components(new Components()
+                        .addSecuritySchemes(
+                                SECURITY_SCHEME_NAME,
+                                new SecurityScheme()
+                                        .name(SECURITY_SCHEME_NAME)
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")));
+    }
+}
